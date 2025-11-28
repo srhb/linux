7df95df3c965a81e11f15ee25842b57d157ef7da
Bin Du <Bin.Du@amd.com>
2025-11-28 17:19:27 +0800

    media: platform: amd: isp4 video node and buffers handling added
    Isp video implements v4l2 video interface and supports NV12 and YUYV. It
    manages buffers, pipeline power and state. Cherry-picked Sultan's DMA
    buffer related fix from branch v6.16-drm-tip-isp4-for-amd on
    https://github.com/kerneltoast/kernel_x86_laptop.git
    
    Co-developed-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Co-developed-by: Svetoslav Stoilov <Svetoslav.Stoilov@amd.com>
    Signed-off-by: Svetoslav Stoilov <Svetoslav.Stoilov@amd.com>
    Signed-off-by: Bin Du <Bin.Du@amd.com>
    Tested-by: Alexey Zagorodnikov <xglooom@gmail.com>

---
+++ drivers/media/platform/amd/isp4/isp4_video.h
@@ -0,0 +1,65 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2025 Advanced Micro Devices, Inc.
+ */
+
+#ifndef _ISP4_VIDEO_H_
+#define _ISP4_VIDEO_H_
+
+#include <media/videobuf2-memops.h>
+#include <media/v4l2-dev.h>
+
+#include "isp4_interface.h"
+
+struct isp4vid_capture_buffer {
+	/*
+	 * struct vb2_v4l2_buffer must be the first element
+	 * the videobuf2 framework will allocate this struct based on
+	 * buf_struct_size and use the first sizeof(struct vb2_buffer) bytes of
+	 * memory as a vb2_buffer
+	 */
+	struct vb2_v4l2_buffer vb2;
+	struct isp4if_img_buf_info img_buf;
+	struct list_head list;
+};
+
+struct isp4vid_ops {
+	int (*send_buffer)(struct v4l2_subdev *sd,
+			   struct isp4if_img_buf_info *img_buf);
+};
+
+struct isp4vid_dev {
+	struct video_device vdev;
+	struct media_pad vdev_pad;
+	struct v4l2_pix_format format;
+
+	/* mutex that protects vbq */
+	struct mutex vbq_lock;
+	struct vb2_queue vbq;
+
+	/* mutex that protects buf_list */
+	struct mutex buf_list_lock;
+	struct list_head buf_list;
+
+	u32 sequence;
+	bool stream_started;
+
+	struct media_pipeline pipe;
+	struct device *dev;
+	struct v4l2_subdev *isp_sdev;
+	struct v4l2_fract timeperframe;
+
+	/* Callback operations */
+	const struct isp4vid_ops *ops;
+};
+
+int isp4vid_dev_init(struct isp4vid_dev *isp_vdev,
+		     struct v4l2_subdev *isp_sdev,
+		     const struct isp4vid_ops *ops);
+
+void isp4vid_dev_deinit(struct isp4vid_dev *isp_vdev);
+
+void isp4vid_handle_frame_done(struct isp4vid_dev *isp_vdev,
+			       const struct isp4if_img_buf_info *img_buf);
+
+#endif /* _ISP4_VIDEO_H_ */
